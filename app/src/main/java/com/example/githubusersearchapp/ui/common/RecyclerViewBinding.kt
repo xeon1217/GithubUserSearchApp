b7c8f25468c36ec3614ad621c8e5b35fae807a21
xeon1 <xeon1217@gmail.com>
2021-06-01 09:27:34 +0900

    dataBinding에 사용되는 바인딩 어댑터 생성

---
+++ app/src/main/java/com/example/githubusersearchapp/ui/common/RecyclerViewBinding.kt
@@ -0,0 +1,14 @@
+package com.example.githubusersearchapp.ui.common
+
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+@Suppress("UNCHECKED_CAST")
+@BindingAdapter("listData")
+fun <T, VH : RecyclerView.ViewHolder> setItem(recyclerView: RecyclerView, items: List<T>?) {
+    (recyclerView.adapter as? ListAdapter<T, VH>)?.let { adapter ->
+        adapter.submitList(items)
+        adapter.notifyDataSetChanged()
+    }
+}
